ccfc25320f0e3945650f0a022a52501bfb1680c2
Fred <leferpz@gmail.com>
2025-04-21 11:23:11 -0400

    Login page added and styled

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -3,19 +3,22 @@ import LoginPage from "./pages/LoginPage"
 import ProfilePage from "./pages/ProfilePage"
 import MessagesPage from "./pages/MessagesPage"
 import { Route, Routes, BrowserRouter } from 'react-router'
+import { Provider } from "./components/ui/provider"
 import './App.css'
 
 function App() {
 
   return (
-    <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<HomePage/>}/>
-            <Route path="/login" element={<LoginPage/>}/>
-            <Route path="/profile" element={<ProfilePage/>}/>
-            <Route path="/messages" element ={<MessagesPage/>}/>
-        </Routes>
-    </BrowserRouter>
+    <Provider>
+      <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<HomePage/>}/>
+              <Route path="/login" element={<LoginPage/>}/>
+              <Route path="/profile" element={<ProfilePage/>}/>
+              <Route path="/messages" element ={<MessagesPage/>}/>
+          </Routes>
+      </BrowserRouter>
+    </Provider>
   )
 }
 
--- apps/frontend/src/pages/LoginPage.tsx
+++ apps/frontend/src/pages/LoginPage.tsx
@@ -1,5 +1,33 @@
+import { Box, Stack, Flex, Button, Field, Input, Text, Separator } from "@chakra-ui/react"
+import { PasswordInput } from "../components/ui/password-input"
+
 export default function LoginPage() {
+
+    function onSubmit(e: any) {
+        e.preventDefault();
+        return
+    }
+
     return (
-        <div>LoginPage</div>
+        <Flex flexDirection="column">
+            <Box boxShadow="lg" bg="blackAlpha.900" p={6} borderRadius="md">
+                <Field.Root>
+                    <form onSubmit={onSubmit}>
+                        <Stack gap="3" maxW="sm">
+                            <Field.Label htmlFor="username">Username</Field.Label>
+                            <Input placeholder="Jenkins123" name="username"></Input>
+                            <Field.Label htmlFor="password">Password</Field.Label>
+                            <PasswordInput name="password"></PasswordInput>
+                            <Button border="0.5px solid lightgreen" color="whiteAlpha.900" type="submit">Login</Button>
+                        </Stack>
+                    </form>
+                </Field.Root>
+                <Separator marginBottom="10px" marginTop="15px" size="md"/>
+                <Stack maxW="sm">
+                        <Text>Don't have an account?</Text>
+                        <Text _hover={{cursor: "pointer", color: "blue.400"}} marginTop="-10px" color="blue.500">Sign up here!</Text>
+                </Stack>
+            </Box>
+        </Flex>
     )
 }
